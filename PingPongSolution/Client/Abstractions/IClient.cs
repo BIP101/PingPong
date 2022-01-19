5fdc2e26e143bf2949c8787dec86b9041d42d81c
BIP101 <yoni280703@gmail.com>
2022-01-19 15:56:50 +0200

    add ICLient interface

---
+++ PingPongSolution/Client/Abstractions/IClient.cs
@@ -0,0 +1,19 @@
+﻿using Common;
+using Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Abstractions
+{
+    public interface IClient<T>
+    {
+        string Name { get; }
+        void ConnectToServer(ServerInfo serverInfo);
+        void SendInfo(IInfo<T> infoToSend);
+        void ParseInfo(T infoToParse);
+        T GetInfo();
+    }
+}
---
+++ PingPongSolution/Common/StringInfo.cs
@@ -0,0 +1,19 @@
+﻿using Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class StringInfo : IInfo<string>
+    {
+        public string Information { get; private set; }
+
+        public StringInfo(string information)
+        {
+            Information = information;
+        }
+    }
+}
