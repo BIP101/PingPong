fcadbce67a5bcc82e37c6cb7c228a62ea34b13b5
BIP101 <yoni280703@gmail.com>
2022-01-19 14:36:45 +0200

    Add C# Project Files

---
+++ PingPongSolution/PingPongSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PingPongSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
