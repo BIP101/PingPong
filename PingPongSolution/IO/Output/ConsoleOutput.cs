455430028a60279c871776493408a4ee8233ea3b
BIP101 <yoni280703@gmail.com>
2022-01-19 15:35:04 +0200

    add ConsoleOutput

---
+++ PingPongSolution/IO/Output/ConsoleOutput.cs
@@ -0,0 +1,27 @@
+﻿using IO.Output.Abstractions;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO.Output
+{
+    public class ConsoleOutput<T> : IOutput<T>
+    {
+        private ILog _logger;
+
+        public ConsoleOutput(ILog logger)
+        {
+            _logger = logger;
+        }
+
+        public void DisplayOutput(T output)
+        {
+            var outputStringFormat = output.ToString();
+            _logger.Debug($"Displaying output, Output: {outputStringFormat}");
+            Console.WriteLine(outputStringFormat);
+        }
+    }
+}
