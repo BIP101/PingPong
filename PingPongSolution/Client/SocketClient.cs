eb9c4359676f2eab2861a4eaa1ba5624e3338ed9
BIP101 <yoni280703@gmail.com>
2022-01-19 16:04:24 +0200

    add socketClient class

---
+++ PingPongSolution/Client/SocketClient.cs
@@ -0,0 +1,41 @@
+﻿using Client.Abstractions;
+using Common;
+using Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class SocketClient : IClient<string>
+    {
+        public string Name { get; private set; }
+
+        public SocketClient(string name)
+        {
+            Name = name;
+        }
+
+        public void ConnectToServer(ServerInfo serverInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ParseInfo(string infoToParse)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendInfo(IInfo<string> infoToSend)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
